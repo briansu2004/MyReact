9cb9453fb3b704ed50c1f2abdd07c4d27d74ae4a
Brian Su <briansu2004@hotmail.com>
2022-12-29 21:27:09 -0500

    $

--- react-springboot-studentsystem/student-backend/src/main/java/com/sutek/studentsystem/controller/StudentController.java
+++ react-springboot-studentsystem/student-backend/src/main/java/com/sutek/studentsystem/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.sutek.studentsystem.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import com.sutek.studentsystem.service.StudentService;
 
 @RestController
 @RequestMapping("/student")
+@CrossOrigin
 public class StudentController {
     @Autowired
     private StudentService studentService;
